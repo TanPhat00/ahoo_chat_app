3751c49de785d1a4b6a2cf51afff9c903f311540
TanPhat00 <tencungkho567@gmail.com>
2025-07-09 18:55:01 +0700

    ≡ƒöä Auto commit at Wed 07/09/2025 18:55:00.98

--- backend/middlewares/upload.middleware.js
+++ backend/middlewares/upload.middleware.js
@@ -1,21 +1,16 @@
+// middleware/upload.middleware.js
 const multer = require('multer');
-
-// Lưu file vào bộ nhớ RAM (dùng cho cloudinary)
 const storage = multer.memoryStorage();
-
-// Chấp nhận các định dạng ảnh cụ thể
 const allowedTypes = ['image/jpeg', 'image/png', 'image/webp', 'image/gif', 'image/jpg'];
 
 const upload = multer({
   storage,
-  limits: { fileSize: 10 * 1024 * 1024 }, // Tối đa 10MB
+  limits: { fileSize: 10 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
-    console.log('[DEBUG] Nhận file:', file.originalname, file.mimetype);
-    upload.single('avatar')
     if (allowedTypes.includes(file.mimetype)) {
       cb(null, true);
     } else {
-      cb(new Error('Chỉ chấp nhận các định dạng hình ảnh: JPEG, PNG, WEBP, GIF'));
+      cb(new Error('Chỉ chấp nhận file ảnh JPG, PNG, WEBP, GIF'));
     }
   }
 });
